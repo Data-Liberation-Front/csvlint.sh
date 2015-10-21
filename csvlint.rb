15944e483e650a9abda17d209500d3fd4e04d967
pezholio <pezholio@gmail.com>
2015-10-21 07:44:15 +0100

    Run gem binary directly

--- csvlint.rb
+++
@@ -1,9 +0,0 @@
-$:.unshift File.join( File.dirname(__FILE__), "..", "lib")
-
-require 'csvlint/cli'
-
-if ARGV == ["help"]
-  Csvlint::Cli.start(["help"])
-else
-  Csvlint::Cli.start(ARGV.unshift("validate"))
-end
--- packaging/wrapper.sh
+++ packaging/wrapper.sh
@@ -1,13 +1,16 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -e
 
 # Figure out where this script is located.
 SELFDIR="`dirname \"$0\"`"
 SELFDIR="`cd \"$SELFDIR\" && pwd`"
 
+RUBY_VERSION="2.1.0"
+CSVLINT_VERSION="0.2.4"
+
 # Tell Bundler where the Gemfile and gems are.
 export BUNDLE_GEMFILE="$SELFDIR/lib/vendor/Gemfile"
 unset BUNDLE_IGNORE_CONFIG
 
 # Run the actual app using the bundled Ruby interpreter, with Bundler activated.
-exec "$SELFDIR/lib/ruby/bin/ruby" -rbundler/setup "$SELFDIR/lib/app/csvlint.rb" $*
+exec "$SELFDIR/lib/ruby/bin/ruby" -rbundler/setup "$SELFDIR/lib/vendor/ruby/$RUBY_VERSION/gems/csvlint-$CSVLINT_VERSION/bin/csvlint" $*
